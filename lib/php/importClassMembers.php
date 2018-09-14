f89dd1a39a8b9654f73cf6281fb8c2a8f668ec87
Jordy van Domselaar <jordy@deinternetjongens.nl>
2018-09-14 22:50:36 +0200

    Refactor to function

--- lib/php/importClassMembers.php
+++ lib/php/importClassMembers.php
@@ -3,32 +3,38 @@ require_once __DIR__."/../../vendor/autoload.php";
 
 // $activeFilePath = $argv[1];
 $activeFilePath = "/Users/jordyvandomselaar/workspace/atom/php-tools/test-files/test.php";
-$parsedFile = new \Go\ParserReflection\ReflectionFile($activeFilePath);
+$classConstructorParameters = getClassConstructorParameters($activeFilePath);
 
+/**
+ * Get all classes with the parameters in their constructors.
+ * @param  string $filePath File to scan for classe.s
+ * @return array            Array of classes with parameters.
+ */
+function getClassConstructorParameters(string $filePath): array
+{
+    $parsedFile = new \Go\ParserReflection\ReflectionFile($activeFilePath);
 
-// Get all construct arguments and their types
-$classConstructorParameters = array_reduce($parsedFile->getFileNamespaces(), function ($carrier, $namespace) {
-    // New array with namespaces as keys, and classes in the namespaces as values
-    return array_reduce($namespace->getClasses(), function ($carrierA, $class) {
-        // Array with class as key and every method as value
-        $carrierA[$class->getName()] = array_reduce($class->getMethods(), function ($carrierB, $method) {
-            // We only want the constructors
-            if ($method->getName() !== "__construct") {
-                return $carrierB;
-            }
-            // Array with parameters as keys and name + type of parameter as values
-            return array_reduce($method->getParameters(), function ($carrierC, $parameter) {
-                    $carrierC['constructor'][$parameter->getName()] = [
-                        'name' => $parameter->getName(),
-                        'type' => (string)$parameter->getType()
-                    ];
+    return array_reduce($parsedFile->getFileNamespaces(), function ($carrier, $namespace) {
+        // New array with namespaces as keys, and classes in the namespaces as values
+        return array_reduce($namespace->getClasses(), function ($carrierA, $class) {
+            // Array with class as key and every method as value
+            $carrierA[$class->getName()] = array_reduce($class->getMethods(), function ($carrierB, $method) {
+                // We only want the constructors
+                if ($method->getName() !== "__construct") {
+                    return $carrierB;
+                }
+                // Array with parameters as keys and name + type of parameter as values
+                return array_reduce($method->getParameters(), function ($carrierC, $parameter) {
+                        $carrierC['constructor'][$parameter->getName()] = [
+                            'name' => $parameter->getName(),
+                            'type' => (string)$parameter->getType()
+                        ];
 
-                    return $carrierC;
-            }, ['constructor' => []]);
+                        return $carrierC;
+                }, ['constructor' => []]);
+            }, []);
+
+            return $carrierA;
         }, []);
-
-        return $carrierA;
     }, []);
-}, []);
-
-var_export($classConstructorParameters);
+}
